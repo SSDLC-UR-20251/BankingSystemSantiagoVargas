f283cb5c4e2dcef2c854d55782d975eb4617b87e
David Santiago Vargas Ovalle <112956569+davinchooo@users.noreply.github.com>
2025-04-12 11:24:03 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
 
 from paramiko.client import SSHClient, AutoAddPolicy, RejectPolicy
 
-def unsafe_connect():
+def safe_connect():
     client = SSHClient()
-    client.set_missing_host_key_policy(AutoAddPolicy)
+    client.set_missing_host_key_policy(RejectPolicy)
     client.connect("example.com")
 
     # ... interaction with server
